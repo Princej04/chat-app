5156a5b2f3556b7577d76cd5871c38ecf06f9166
pj1322111@gmail.com <pj1322111@gmail.com>
2026-06-11 11:30:18 +0530

    fix: add real vercel url to cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,7 +12,7 @@ const cors = require('cors');
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://your-app.vercel.app' // We will update this exact string later!
+    'https://chat-app-iota-rouge.vercel.app/' // updated
   ]
 }));
 
